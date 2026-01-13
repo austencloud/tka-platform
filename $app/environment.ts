dfee5cd2260d54e9f86efd8e1ed5a9edaf71fa3c
Austen Cloud <austencloud@gmail.com>
2026-01-13 05:50:44 -0600

    chore: remove accidentally committed temp files

--- $app/environment.ts
+++
@@ -1,8 +0,0 @@
-/**
- * SvelteKit $app/environment stub for Node.js CLI
- */
-
-export const browser = false;
-export const building = false;
-export const dev = false;
-export const version = 'node-cli';
--- $app/stores.ts
+++
@@ -1,8 +0,0 @@
-/**
- * SvelteKit $app/stores stub for Node.js CLI
- */
-
-// Return stub stores that do nothing
-export const page = { subscribe: () => () => {} };
-export const navigating = { subscribe: () => () => {} };
-export const updated = { subscribe: () => () => {} };
